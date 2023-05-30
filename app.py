7f6d72d945ba1f89ae4bb8978c85e8166006a1b5
Akshat Sehgal <akshatsehgal26@gmail.com>
2023-05-30 18:49:18 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+from NLP_Summarizer import summarizer
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template("index.html")
+
+@app.route ('/analyze', methods=['GET','POST'])
+def analyze():
+    if request.method == 'POST':
+        rawtext = request.form['rawtext']
+        sum_perc=int(request.form.get("Summ_Percent"))
+        print(sum_perc)
+        summary, original_txt, len_original_txt, len_summary = summarizer(rawtext,sum_perc)
+
+    return render_template('summary.html', summary = summary, original_txt = original_txt, len_original_txt = len_original_txt, len_summary = len_summary)
+if __name__ == "__main__":
+    app.run(debug = True)
