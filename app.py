bdd09e02bbda3f921224a633aecc574e4ffdcf6c
Manish Nayak <nayak10softworld@gmail.com>
2024-07-24 09:01:40 +0530

    Text_Summarizer using nlp in python

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 #hello!
 from NLP_Summarizer import summarizer
+
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/') 
+ 
 def index():
     return render_template("index.html")
 
@@ -16,6 +17,8 @@ def analyze():
         print(sum_perc)
         summary, original_txt, len_original_txt, len_summary = summarizer(rawtext,sum_perc)
 
-    return render_template('summary.html', summary = summary, original_txt = original_txt, len_original_txt = len_original_txt, len_summary = len_summary)
+    return render_template('summary.html', summary=summary, original_txt=original_txt, len_original_txt=len_original_txt, len_summary=len_summary)
 if __name__ == "__main__":
     app.run(debug = True)
+  
+  
